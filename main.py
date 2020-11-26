d0b19713d969784353c5522f3b7879422b0c7eef
Chenhjseu <chenhj.seu@foxmail.com>
2020-11-26 15:49:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
-import face_recognition
-import update
-import prediction
-import feedback
-import play_the_song
+# This is a sample Python script.
+import Initialize
+from update_rating import *
+import load_data
+# import prediction
+import pandas as pd
+import numpy as np
+from tkinter import *
+import time
 
-if user is new:
-  initialize()
+if __name__ == '__main__':
 
-while True:
-  emotion_state = face_recognition(self)
-  if emotion_state == 1:
-    ratings = pd.read_csv(ratings_positive.csv)
-  elif emotion_state == 2:
-    ratings = pd.read_csv(ratings_negative.csv)
-  else:
-    ratings = pd.read_csv(ratings_nature.csv)
+    my_db = my_mysql()
+    user_df = my_db.Read_database('')
 
-  recommend_playlist = prediction(ratings)
+    name_list = user_df['User Name']
 
-  play_the_song(recommend_playlist)
+    window = Tk()
+    window.title("Login")
+    window.geometry("350x200")
+    lbl = Label(window, text="User Name:")
+    lbl.grid(column=0, row=0)
+    txt = Entry(window, width=10)
+    txt.grid(column=1, row=0)
 
-  new_rating = feedback
 
-  update(new_rating) 
+    def clicked ():
+        lbl.configure(text="Successfully Login!")
+        window.quit()
 
-  emotion_state = face_recognition(self)
+
+    btn = Button(window, text="Enter", command=clicked)
+    btn.grid(column=2, row=0)
+    window.mainloop()
+
+    user_name = txt.get()
+
+    if user_name not in name_list:
+        Initialize.get_ratings()
+
+    while 1:
+        emotion_state = get_emotion()
+
+        recommend_song_id = prediction(user_name, emotion_state)
+
+        feedback = get_feedback()
+
+        current_emotion_state = get_emotion()
+
+        update_rating(feedback, current_emotion_state)
